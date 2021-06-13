18cbf8339cac2880514754377d00ebdcfa81c64a
phiokari <phiokari@gmail.com>
2021-06-13 12:34:32 +0900

    無念の撤退

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import datetime
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,6 +16,13 @@ def monomane():
 def monomane_result():
     return render_template('result.html')
 
+@app.route('/monomane', methods=['POST'])
+def uploaded_wav():
+    fname = "../static/data/" + datetime.now().strftime('%m%d%H%M%S') + ".wav"
+    with open(f"{fname}", "wb") as f:
+        f.write(request.files['data'].read())
+    print(f"posted sound file: {fname}")
+    return jsonify({"data": fname})
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
